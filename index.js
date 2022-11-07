fc76fc0c5662efbf1075f73b3bc76104dba114f7
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-07 14:16:07 +0545

    more node express routes configured through DB

--- index.js
+++ index.js
@@ -57,6 +57,23 @@ App.post("/notes/", (request, response) => {
   // response.status(204).end();
   response.status(201).json(myIncomingData);
 });
+// App.post("/api/notes", (request, response) => {
+//   const body = request.body;
+
+//   if (body.content === undefined) {
+//     return response.status(400).json({ error: "content missing" });
+//   }
+
+//   const note = new Note({
+//     content: body.content,
+//     important: body.important || false,
+//     date: new Date(),
+//   });
+
+//   note.save().then((savedNote) => {
+//     response.json(savedNote);
+//   });
+// });
 App.put("/notes/:id", (request, response, next) => {
   const body = request.body;
 
