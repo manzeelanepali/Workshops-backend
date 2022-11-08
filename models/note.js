411ebcd3466498b4ad8e183742cd35dd9fa6fdff
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-08 15:36:30 +0545

    feat:setting up an environmental variables

--- models/note.js
+++ models/note.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-
-const url = process.env.MONGODB_URI;
+let url;
+if (process.env.NODE_ENV === "DEV") {
+  url = process.env.MONGODB_URI_PROD;
+} else {
+  process.env.NODE_Env.MONGODB_URI_DEV;
+}
 
 console.log("connecting to", url);
 
