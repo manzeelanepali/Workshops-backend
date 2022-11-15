07c1db4070ed6f9bc5d24b14b03da8c26254e240
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-15 16:42:52 +0545

    added a test on the note_api.test that says added a valid note

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -45,7 +45,25 @@ test("a specific note is within the returned notes", async () => {
   const contents = response.body.map((r) => r.content);
   expect(contents).toContain("Browser can execute only Javascript");
 });
+test("a valid note can be added", async () => {
+  const newNote = {
+    content: "async/await simplifies making async calls",
+    important: true,
+  };
 
+  await api
+    .post("/api/notes")
+    .send(newNote)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/notes");
+
+  const contents = response.body.map((r) => r.content);
+
+  expect(response.body).toHaveLength(initialNotes.length + 1);
+  expect(contents).toContain("async/await simplifies making async calls");
+});
 afterAll(() => {
   mongoose.connection.close();
 });
