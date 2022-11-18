8567f30fa1dc79887552019433e1a217bf6feff7
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-18 11:45:45 +0545

    feat : another test cases added .and passed

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,12 @@ const User = require("../models/user");
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return response.status(400).json({
+      error: "username must be unique",
+    });
+  }
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
