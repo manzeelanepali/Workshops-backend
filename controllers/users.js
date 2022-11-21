7fff1bb38755e5ea93a7dbc62d68d1ebf377c8aa
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-21 15:42:52 +0545

    feat : test loggin in using VS code REST-client

--- controllers/users.js
+++ controllers/users.js
@@ -29,6 +29,7 @@ usersRouter.get("/", async (request, response) => {
   const users = await User.find({}).populate("notes", {
     content: 1,
     important: 1,
+    date: 1,
   });
   response.json(users);
 });
