21e3975cf8592765374916926b1d539878c7619a
manzeelanepali <nepalimanzeela@gmail.com>
2022-11-22 13:57:49 +0545

    feat: catching the error if wronng token is given . use try catch method in the notes.js of the controller

--- controllers/notes.js
+++ controllers/notes.js
@@ -33,38 +33,37 @@ const getTokenFrom = (request) => {
 
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
+  try {
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
+    // console.log(" This is a decoded Token ", decodedToken);
+    const user = await User.findById(decodedToken.id);
+    // console.log(" This is my user", user);
 
-  const token = getTokenFrom(request);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
-  }
-  console.log(" This is a decoded Token ", decodedToken);
-  const user = await User.findById(decodedToken.id);
-  console.log(" This is my user", user);
+    if (!body.content) {
+      return response.status(400);
+    } else {
+      const currentUser = await User.findById(user._id);
+      console.log("this is user", currentUser);
+      const note = new Note({
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        user: user._id,
+      });
+      // console.log(note);
 
-  if (!body.content) {
-    return response.status(400);
-  } else {
-    const currentUser = await User.findById(user._id);
-    console.log("this is user", currentUser);
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      user: user._id,
-    });
-    console.log(note);
-
-    try {
       const newNote = await note.save();
       user.notes.concat(newNote._id);
       user.save();
 
       response.status(201).json(newNote);
-    } catch (error) {
-      next(error);
     }
+  } catch (error) {
+    next(error);
   }
 });
 
